5233faa81004776d8504b54d9ce34a9f589e9975
Tej Lehal <tejlehal13@gmail.com>
2024-11-04 15:55:31 -0500

    Initial Commit - Added routes, connect Neon and added Clerk for auth

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// const isDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
+// const isAdminRoute = createRouteMatcher(['/admin(.*)'])
+
+// export default clerkMiddleware(async (auth, req) => {
+//   // Restrict admin route to users with specific role
+//   if (isAdminRoute(req)) await auth.protect({ role: 'org:admin' })
+
+//   // Restrict dashboard routes to signed in users
+//   if (isDashboardRoute(req)) await auth.protect()
+// })
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
